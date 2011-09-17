a1086029907c4bbad0031ae4374a6ec7145d4c07
xaicron <xaicron@gmail.com>
2011-09-17 17:59:29 +0900

    initial commit

---
+++ lib/Test/Level.pm
@@ -0,0 +1,42 @@
+package Test::Level;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Test::Level -
+
+=head1 SYNOPSIS
+
+  use Test::Level;
+
+=head1 DESCRIPTION
+
+Test::Level is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2011 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
